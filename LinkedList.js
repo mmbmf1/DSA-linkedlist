14be7cc26399b82ca3f6dbb918172ff4606886bb
Michael <mmbmf.mm@gmail.com>
2020-03-02 19:40:43 -0600

    initial commit

---
+++ LinkedList.js
@@ -0,0 +1,9 @@
+// Create a linked list class
+// Walk through the linked list code in the curriculum and understand it well. Then write a linked list class and its core functions (insertFirst, insertLast, remove, find) from scratch.
+
+class _Node {
+  constructor(value, next) {
+    this.value = value;
+    this.next = next;
+  }
+}
